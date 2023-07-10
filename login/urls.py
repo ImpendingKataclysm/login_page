1195f7759e96e46055561dfb7aedd02b76f5f34b
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-07-10 13:18:36 -0700

    Added a login page

--- login/urls.py
+++ login/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='login')
+    path('accounts/', views.login, name='login'),
+    path('', views.index, name='home'),
 ]
